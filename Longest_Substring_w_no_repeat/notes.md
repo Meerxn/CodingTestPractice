cb3181fee2a908987aee1bdb6c9aaab4c88a9328
Meerxn <fardeenfmeeran@gmail.com>
2020-08-26 23:15:24 +0400

    solutions and notes version 1

---
+++ Longest_Substring_w_no_repeat/notes.md
@@ -0,0 +1,6 @@
+<h1> My notes </h1>
+
+Runtime: 1048 ms, faster than 5.01% of Java online submissions for Longest Substring Without Repeating Characters.
+Memory Usage: 40.4 MB, less than 44.05% of Java online submissions for Longest Substring Without Repeating Characters.
+
+I did not realize hasmaps were optimizable here hence I will redo this solution / implmentation using hashmaps. These results proved that my brute force methods cause the run time to increase way too much. 
---
+++ Longest_Substring_w_no_repeat/solution.java
@@ -0,0 +1,45 @@
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int longest = 0;
+        
+       
+        for (int i = 0 ; i < s.length() ; i ++){
+            int count = 0 ; 
+             ArrayList<String> str = new ArrayList<String>();
+             str.add(s.substring(i,i+1));
+            for (int j = i+1 ; j < s.length() ; j++){
+                if (str.contains(s.substring(j,j+1))){
+                    count++;
+                    break;
+                }
+                else {
+                    str.add(s.substring(j,j+1));
+                }
+            }
+            if (str.size() > longest){
+                longest = str.size();
+            }
+        }
+        
+        
+        
+        
+        
+        
+
+        
+           if (s.length() > 0 && longest == 0  ){
+               return 1;
+       }
+        
+        else{
+         return longest;
+               
+        } 
+        
+            
+        }
+        
+        
+    }

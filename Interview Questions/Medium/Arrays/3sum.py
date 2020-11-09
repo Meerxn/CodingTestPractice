8590ee00a2b718c55812ff53682d7e64858e11bb
Meerxn <fardeenfmeeran@gmail.com>
2020-11-09 23:06:38 +0400

    comments added

--- Interview Questions/Medium/Arrays/3sum.py
+++ Interview Questions/Medium/Arrays/3sum.py
@@ -9,14 +9,20 @@
 def threeSum(nums):
     nums = sorted(nums)
     ans = [] 
+    #defaul check on empty strings
     if len(nums) < 3:
         return []
+    #making sure to not need to loop ahead    
     for i in range(len(nums) -2):
+        #positive integer start means everything ahead is also posotive so no need
         if nums[i] > 0:
             return ans
+        #Here we check whether every element is not repeated again (checking for a position repeats)
         if nums[i] == nums[i-1] and i > 0:
-            continue    
+            continue  
+        #left  
         b = i+1
+        #rifht
         c = len(nums) -1
         while b < c:
             sumV = nums[i] + nums[b] + nums[c]
@@ -35,4 +41,4 @@ def threeSum(nums):
         
     return ans
         
-print(threeSum([-1,0,1,2,-1,-4]))        
+print(threeSum([-1,0,1,2,-1,-4]))

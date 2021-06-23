63f6872130945333f37f02452ccd5ca49ef81216
Meerxn <fardeenfmeeran@gmail.com>
2021-06-24 01:40:41 +0400

    Max Depth

---
+++ Interview Questions/Easy/Trees/maxDeptOfTree.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+            if not root:
+                return 0
+            return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+            
+        
+        
